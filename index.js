9bdf61ae5a505f2eb6c21dc436697fd8b1d5bf9d
Kaan Aydeniz <kaanayden15@gmail.com>
2024-09-09 04:13:18 +0300

    add inital file server

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const storageRouter = require('./routers/storageRouter');
+
+const app = express()
+const port = 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('This is image server!')
+})
+
+app.use('/public', express.static('public'))
+app.use('/storage', storageRouter)
+  
+app.listen(port, () => {
+  console.log(`Image server listening on port ${port}`)
+})
