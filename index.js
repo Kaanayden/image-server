2a7eab220bb82e3c96f71e67542500350c4553b1
Kaanayden <kaanayden15@gmail.com>
2024-09-09 04:21:18 +0300

    fix port bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const storageRouter = require('./routers/storageRouter');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8000
 
 
 app.use(cors());
